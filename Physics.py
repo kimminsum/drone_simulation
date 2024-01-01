b0c796ba3b60a72e91b6c2dccb522551229bc446
kimminsum <tommy1005a@gmail.com>
2024-01-01 23:37:47 +0900

    boost node

--- Physics.py
+++ Physics.py
@@ -3,36 +3,51 @@ import pygame, sys, math
 pygame.init()
 
 FPS = 60 # Frames per second setting
+Title = "Drone Project"
+GRAVITY = 9.8 * 2
 fpsClock = pygame.time.Clock()
 
-# Set up the window
-WIDTH = 400
-HEIGHT = 300
-screen = pygame.display.set_mode((WIDTH, HEIGHT), 0, 32)
-pygame.display.set_caption('Drone Project')
+# Set up the window 
+WIDTH = 500
+HEIGHT = 500
 
 Colour = {
-    "BLACK" : (40, 40, 40),
-    "WHITE" : (255, 255, 255),
-    "RED" : (255, 0, 0),
-    "ORANGE" : (255, 69, 0)
+    "BlACK": (0, 0, 0),
+    "WHITE": (255, 255, 255),
+    "RED": (255, 0, 0)
 }
 
-class Particle:
-    def __init__(self, x, y, m = 1.0):
-        self.m = m
+Graph = [
+    [0, 1, 1, 1, 1, 0],
+    [1, 0, 1, 1, 1, 1],
+    [1, 1, 0, 1, 1, 1],
+    [1, 1, 1, 0, 0, 1],
+    [1, 1, 1, 0, 0, 1],
+    [0, 1, 1, 1, 1, 0]
+]
+
+screen = pygame.display.set_mode((WIDTH, HEIGHT), 0, 32)
+pygame.display.set_caption(Title)
+
+"""
+Node
+"""
+class Node:
+    def __init__(self, x, y, colour):
         self.x = x
         self.y = y
-        self.oldx = x-10
+        self.oldx = x
         self.oldy = y
         self.newx = x
         self.newy = y
         self.ax = 0
-        self.ay = 9.8
-        
+        self.ay = GRAVITY
+
+        self.colour = colour
+
         self.selected = False
         self.fixed = False
-        
+
     def update(self, delta_t):
         if self.fixed == False:
             # Collision Process
@@ -40,7 +55,7 @@ class Particle:
                 self.x, self.oldx = self.oldx, self.x
             if self.y < 0 or self.y > HEIGHT:
                 self.y, self.oldy = self.oldy, self.y
-    
+
             # Verlet Integration
             self.newx = 2.0 * self.x - self.oldx + self.ax * delta_t * delta_t 
             self.newy = 2.0 * self.y - self.oldy + self.ay * delta_t * delta_t 
@@ -53,91 +68,102 @@ class Particle:
             pos = pygame.mouse.get_pos()
             self.x = pos[0]
             self.y = pos[1]
-            
+
         if mouse == False:
             self.selected = False
-        
+
     def draw(self, surf, size):
         if self.selected == True:
-            color = Colour["ORANGE"]
+            color = self.colour[1]
         else:
-            color = Colour["WHITE"]
+            color = self.colour[0]
         pygame.draw.circle(surf, color, (int(self.x), int(self.y)), size)
-        
+"""
+Constraint -> trunk
+"""
 class Constraint:
     def __init__(self, index0, index1):
         self.index0 = index0
         self.index1 = index1
-        delta_x = particles[index0].x - particles[index1].x
-        delta_y = particles[index0].y - particles[index1].y
+        delta_x = Nodes[index0].x - Nodes[index1].x
+        delta_y = Nodes[index0].y - Nodes[index1].y
         self.restLength = math.sqrt(delta_x * delta_x + delta_y * delta_y)
 
     def update(self):
-        delta_x = particles[self.index1].x - particles[self.index0].x
-        delta_y = particles[self.index1].y - particles[self.index0].y
+        delta_x = Nodes[self.index1].x - Nodes[self.index0].x
+        delta_y = Nodes[self.index1].y - Nodes[self.index0].y
         deltaLength = math.sqrt(delta_x * delta_x + delta_y * delta_y)
         diff = (deltaLength - self.restLength)/(deltaLength + 0.001)
 
-        if particles[self.index0].fixed == False:
-            particles[self.index0].x += 0.5 * diff * delta_x
-            particles[self.index0].y += 0.5 * diff * delta_y
-        if particles[self.index1].fixed == False:
-            particles[self.index1].x -= 0.5 * diff * delta_x
-            particles[self.index1].y -= 0.5 * diff * delta_y
+        if Nodes[self.index0].fixed == False:
+            Nodes[self.index0].x += 0.5 * diff * delta_x
+            Nodes[self.index0].y += 0.5 * diff * delta_y
+        if Nodes[self.index1].fixed == False:
+            Nodes[self.index1].x -= 0.5 * diff * delta_x
+            Nodes[self.index1].y -= 0.5 * diff * delta_y
 
     def draw(self, surf, size):
-        x0 = particles[self.index0].x
-        y0 = particles[self.index0].y
-        x1 = particles[self.index1].x
-        y1 = particles[self.index1].y
+        x0 = Nodes[self.index0].x
+        y0 = Nodes[self.index0].y
+        x1 = Nodes[self.index1].x
+        y1 = Nodes[self.index1].y
         pygame.draw.line(surf, Colour["WHITE"], (int(x0), int(y0)), (int(x1), int(y1)), size)
+
+def find_Node(pos):
+    for i in range(len(Nodes)):
+        dx = Nodes[i].x - pos[0]
+        dy = Nodes[i].y - pos[1]
+        if (dx * dx + dy * dy) < 400:
+            Nodes[i].selected = True
+            break
 
 delta_t = 0.1
 NUM_ITER = 10
 mouse = False
 
-# Create particles
-particles = []
+# Create Nodes
+Nodes = []
 for i in range(4):
-    x = 20.0 * math.cos(math.radians(90) * i)
-    y = 20.0 * math.sin(math.radians(90) * i)
-    p = Particle(WIDTH * 0.5 + x, HEIGHT * 0.5 + y)
+    x = 40.0 * math.cos(math.radians(90) * i + math.radians(45))
+    y = 40.0 * math.sin(math.radians(90) * i + math.radians(45))
     if i == 0:
+        p = Node(WIDTH * 0.5 + x, HEIGHT * 0.9 + y, (Colour["RED"], Colour["RED"]))
         p.fixed = False
-    particles.append(p)
+    else:
+        p = Node(WIDTH * 0.5 + x, HEIGHT * 0.9 + y, (Colour["WHITE"], Colour["RED"]))
+    Nodes.append(p)
 
-def find_particle(pos):
-    for i in range(len(particles)):
-        dx = particles[i].x - pos[0]
-        dy = particles[i].y - pos[1]
-        if (dx*dx + dy*dy) < 400:
-            particles[i].selected = True
-            break
+x = 28.28427 * 3
+y = 28.28427
+p = Node(WIDTH * 0.5 - x, HEIGHT * 0.9 - y, (Colour["WHITE"], Colour["RED"]))
+Nodes.append(p)
+
+x = 28.28427 * 3
+y = -28.28427
+p = Node(WIDTH * 0.5 - x, HEIGHT * 0.9 - y, (Colour["RED"], Colour["RED"]))
+Nodes.append(p)
 
 constraints = []
-for i in range(4):
-    index0 = i
-    index1 = (i + 1) % 4
-    c = Constraint(index0, index1)
-    constraints.append(c)
 
-constraints.append(Constraint(0, 2))
-constraints.append(Constraint(1, 3))
+for row in range(6):
+    for column in range(row):
+        if (Graph[row][column] == 1):
+            constraints.append(Constraint(row, column))
 
 while True:
-    screen.fill(Colour["BLACK"])
+    screen.fill(Colour["BlACK"])
 
-    # Particles update
-    for i in range(len(particles)):
-        particles[i].update(delta_t)
+    # Nodes update
+    for i in range(len(Nodes)):
+        Nodes[i].update(delta_t)
     # Constraints update
     for n in range(NUM_ITER):
         for i in range(len(constraints)):
             constraints[i].update()
 
-    # Particles draw
-    for i in range(len(particles)):
-        particles[i].draw(screen, 3)
+    # Nodes draw
+    for i in range(len(Nodes)):
+        Nodes[i].draw(screen, 3)
     # Constraints draw
     for i in range(len(constraints)):
         constraints[i].draw(screen, 1)
@@ -154,7 +180,7 @@ while True:
 
     if mouse:
         pos = pygame.mouse.get_pos()
-        find_particle(pos)
+        find_Node(pos)
 
     pygame.display.update()
     fpsClock.tick(FPS)
