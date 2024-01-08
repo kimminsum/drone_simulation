a8c64b63b6bdf127799e7561dd45dcfb2f839658
kimminsum <tommy1005a@gmail.com>
2024-01-08 20:51:42 +0900

    Add comment in code

--- Main.py
+++ Main.py
@@ -7,12 +7,12 @@ pygame.init()
 Title = "Drone Project"
 pygame.display.set_caption(Title)
 
-# Set up the window 
-WIDTH = 500
-HEIGHT = 500
+# Window setup
+WIDTH = 400
+HEIGHT = 600
 screen = pygame.display.set_mode((WIDTH, HEIGHT), 0, 32)
 
-# Frame
+# Clock
 FPS = 60
 fpsClock = pygame.time.Clock()
 
@@ -22,6 +22,7 @@ Drone Structure
 # Colour
 Colour = {
     "BlACK": (0, 0, 0),
+    "GRAY": (50, 50, 50),
     "WHITE": (255, 255, 255),
     "RED": (255, 0, 0)
 }
@@ -39,22 +40,23 @@ Graph = [
 
 Nodes = []
 constraints = []
-
-# Movement
+"""
+Movement
+"""
 GRAVITY = 9.8
 START_HEIGHT = 0.93 # topside is standard
-REACTION = 0.02 # reaction rate; the more bigger, the less reaction
+REACTION = 0.0 # reaction rate; the more bigger, the less reaction
 
-angle = 90 # initial angle
-left_boost = 9.8 * 4
-right_boost = 9.8 * 4
+angle = 0 # initial angle
+left_boost = 9.8 * 3.7
+right_boost = 9.8 * 3.7
 
 delta_t = 0.1
 
 mouse = False # clicked ? True : False
 
 # Font
-font = pygame.font.Font(None, 24)
+font = pygame.font.Font(None, 22)
 """
 Node
 """
@@ -110,9 +112,10 @@ class Node:
 
     def change_boost(self, ax, ay):
         self.ax = ax
-        self.ay = ay # GRAVITY - ay
+        self.ay = ay
 """
 Constraint -> trunk
+connect nodes by graph.
 """
 class Constraint:
     def __init__(self, index0, index1):
@@ -134,15 +137,16 @@ class Constraint:
         if Nodes[self.index1].fixed == False:
             Nodes[self.index1].x -= 0.5 * diff * delta_x
             Nodes[self.index1].y -= 0.5 * diff * delta_y
-
+    # draw line between two points.
     def draw(self, surf, size):
         x0 = Nodes[self.index0].x
         y0 = Nodes[self.index0].y
         x1 = Nodes[self.index1].x
         y1 = Nodes[self.index1].y
         pygame.draw.line(surf, Colour["WHITE"], (int(x0), int(y0)), (int(x1), int(y1)), size)
-
-
+"""
+If clicked, change 'selected' to True.
+"""
 def find_Node(pos):
     for i in range(len(Nodes)):
         dx = Nodes[i].x - pos[0]
@@ -150,14 +154,16 @@ def find_Node(pos):
         if (dx * dx + dy * dy) < 400:
             Nodes[i].selected = True
             break
-
+"""
+Show info on window.
+"""
 def show_info(title: str, data: float, x: int, y: int):
     text = font.render(f"{title}: {data}", True, Colour["WHITE"])
     textRect = text.get_rect()
     textRect.topleft = (x, y)
     screen.blit(text, textRect)
 
-
+# Set nodes' position
 for i in range(4):
     x = 40.0 * math.cos(math.radians(90) * i + math.radians(45))
     y = 40.0 * math.sin(math.radians(90) * i + math.radians(45))
@@ -178,7 +184,7 @@ y = -28.28427
 p = Node(WIDTH * 0.5 - x, HEIGHT * START_HEIGHT - y, (Colour["RED"], Colour["RED"]))
 Nodes.append(p)
 
-
+#  Connect constraints by 'graph'.
 for row in range(6):
     for column in range(row):
         if (Graph[row][column] == 1):
@@ -188,16 +194,19 @@ Rendering
 """
 running = True
 while running:
-    screen.fill(Colour["BlACK"])
+    screen.fill(Colour["GRAY"]) # make background colour black.
 
     # Constraints update
     for n in range(NUM_ITER):
         for i in range(len(constraints)):
             constraints[i].update()
 
+    """
+    Draw objects
+    """
     # Nodes draw
     for i in range(len(Nodes)):
-        Nodes[i].draw(screen, 3)
+        Nodes[i].draw(screen, 4)
     # Constraints draw
     for i in range(len(constraints)):
         constraints[i].draw(screen, 1)
@@ -207,26 +216,27 @@ while running:
 
     # Nodes update
     for i in range(len(Nodes)):
-        # angle
+        # get angle between two nodes; index number 1 and 2.
         if i == 1:
             new_x, new_y = Nodes[i].show_info()
-
         elif i == 2:
             x, y = Nodes[i].show_info()
-            angle = math.atan2(new_y - y, new_x - x) - math.pi/2
+            angle = math.atan2(new_y - y, new_x - x) - math.pi / 2 # get angle
 
-        # right
+        # set right boost vector
         if i == 0:
             vec_x = right_boost * math.sin(angle)
+            # Since the force opposing the Earth's gravity is input, it was calculated by subtracting the value from the gravity. 
             vec_y = GRAVITY - right_boost * math.cos(angle)
             Nodes[i].change_boost(vec_x, vec_y)
-        # left
+        # set left boost vector
         elif i == 5:
             vec_x = left_boost * math.sin(angle)
+            # Since the force opposing the Earth's gravity is input, it was calculated by subtracting the value from the gravity. 
             vec_y = GRAVITY - left_boost * math.cos(angle)
             Nodes[i].change_boost(vec_x, vec_y)
 
-        Nodes[i].update(delta_t)
+        Nodes[i].update(delta_t) # update every change
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -237,14 +247,14 @@ while running:
             mouse = False
 
     if mouse:
-        pos = pygame.mouse.get_pos()
-        find_Node(pos)
+        pos = pygame.mouse.get_pos() # get mouse position
+        find_Node(pos) # clicked change node fixed.
 
-    show_info("Angle", round(math.degrees(angle), 3), 20, 20)
-    show_info("LB", round(left_boost, 3), 20, 40)
-    show_info("RB", round(right_boost, 3), 20, 60)
+    show_info("Angle", round(math.degrees(angle), 3), 20, 20) # show angle infomation; degrees
+    show_info("LB", round(left_boost, 3), 20, 40) # show left boost information
+    show_info("RB", round(right_boost, 3), 20, 60) # show right boost information
 
     pygame.display.update()
     fpsClock.tick(FPS)
 
-pygame.quit()
+pygame.quit() # end up program.
